3f567b1afd90fb08d7faef0598a314aedb35229b
PaxuitoGIT <bxu@myuax.com>
2024-02-21 11:43:07 +0100

    Valores Maximos

---
+++ ValoresMaximos.java
@@ -0,0 +1,8 @@
+public class ValoresMaximos {
+    public void mostrarValoresMaximos() {
+        System.out.println("Valor máximo int: " + Integer.MAX_VALUE);
+        System.out.println("Valor máximo long: " + Long.MAX_VALUE);
+        System.out.println("Valor máximo float: " + Float.MAX_VALUE);
+        System.out.println("Valor máximo double: " + Double.MAX_VALUE);
+    }
+}
