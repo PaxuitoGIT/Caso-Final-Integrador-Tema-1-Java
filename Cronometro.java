a9780c615d6b2d477b0a4892b1cba743c1a28b4f
PaxuitoGIT <bxu@myuax.com>
2024-02-21 11:34:10 +0100

    Mostrar tiempo

--- Cronometro.java
+++ Cronometro.java
@@ -21,7 +21,24 @@ public class Cronometro {
         return convertirHoras(dias * 24);
     }
 
+    public double convertirTiempoPlaneta(double segundosTierra, double factorConversionPlaneta) {
+        return segundosTierra * factorConversionPlaneta;
+    }
+
     public double convertirAnios(double anios) {
         return convertirDias(anios * 365);
+    }
+
+    public void mostrarTiempo(double segundos) {
+        double minutos = segundos / 60;
+        double horas = minutos / 60;
+        double dias = horas / 24;
+        double anios = dias / 365;
+
+        System.out.println("Segundos: " + segundos);
+        System.out.println("Minutos: " + minutos);
+        System.out.println("Horas: " + horas);
+        System.out.print("Dias: " +dias);
+        System.out.println("Años: " +anios);
     }
 }
